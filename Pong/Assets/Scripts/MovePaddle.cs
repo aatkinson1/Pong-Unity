c5bc5883be0c8859170cd54d7b29d7bb126520c0
Nikki <nikkiatkinson1@gmail.com>
2018-01-24 13:56:13 -0600

    First Commit
    Added Unity Project to Github

---
+++ Pong/Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour {
+	
+	public float speed = 30;
+
+	void FixedUpdate(){
+
+		float vertDirection = Input.GetAxisRaw ("Vertical");
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, vertDirection) * speed;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
